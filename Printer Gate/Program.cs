f6c751d6e007e92aba6dbf0731b8de4b48bb0a29
sage2025 <hrm.2021@outlook.com>
2024-01-07 21:54:51 +0200

    printer-gate

---
+++ Printer Gate/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace PrinterGateXP
+{
+	internal static class Program
+	{
+		[STAThread]
+		private static void Main()
+		{
+			using (Mutex mutex = new Mutex(false, "Printer Gate"))
+			{
+				if (!mutex.WaitOne(TimeSpan.Zero))
+				{
+					MessageBox.Show("Another Print Gate instance is running.");
+				}
+				else
+				{
+                    string exePath = @"update.exe";
+                    try
+                    {
+                        Process process = new Process();
+                        // Set the process start information
+                        process.StartInfo.FileName = exePath;
+
+                        // Start the process
+                        process.Start();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error: " + ex.Message);
+                    }
+                    finally
+                    {
+
+                    }
+                    Application.EnableVisualStyles();
+					Application.SetCompatibleTextRenderingDefault(false);
+					//Application.Run(new MainForm());
+					Application.Run(new MainFormAdvanced());
+					mutex.ReleaseMutex();
+				}
+			}
+		}
+	}
+}
